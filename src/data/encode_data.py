cc992a2623f2360cfbc3b9a77b910c37dfcc6551
nfrvnikita <nfrv.nikita@gmail.com>
2024-04-16 22:23:23 +0300

    select models

---
+++ src/data/encode_data.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+
+
+def encode_dataset(df: pd.DataFrame) -> pd.DataFrame:
+    """Encode the input dataset using OneHotEncoder
+
+    Args:
+        df (pd.DataFrame): Input data
+
+    Returns:
+        pd.DataFrame: Encoded dataset
+    """
+    encoder = OneHotEncoder()
+    encoded_df = pd.DataFrame(encoder.fit_transform(df[['age', 'yrs_married', 'occupation',
+                                                        'occupation_husb']]).toarray(),
+                              columns=encoder.get_feature_names_out(['age', 'yrs_married',
+                                                                     'occupation', 'occupation_husb']))
+    df = pd.concat([df.drop(columns=['age', 'yrs_married', 'occupation', 'occupation_husb']), encoded_df], axis=1)
+    return df
