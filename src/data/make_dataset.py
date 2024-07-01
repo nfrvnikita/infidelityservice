0ec0e863f5742a64c984325e66c6f49291bb5390
nfrvnikita <nfrv.nikita@gmail.com>
2024-07-01 23:30:09 +0300

    add convert func, DataFrame -> Tensors

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,47 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+"""Function for obtaining train and test samples after all transformations"""
+import os
+import typing as tp
+import pandas as pd
+import torch
+from dotenv import load_dotenv
+from sklearn.model_selection import train_test_split
+from src.data.encode_data import encode_dataset
+
+load_dotenv()
+RAW_DATA_PATH = os.getenv('RAW_DATA_PATH')
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+def make_dataset(input_data: str) -> tp.Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+    """Function for obtaining traine and test samples after all transformations.
+
+    Args:
+        input_data (pd.DataFrame): Our raw data.
+
+    Returns:
+        tp.Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]: Train and test samples.
     """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    data = pd.read_csv(input_data)
+    encoded_data = encode_dataset(data)
+
+    X = encoded_data.drop(columns=['affairs'])
+    y = encoded_data['affairs']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    return X_train, X_test, y_train, y_test
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+def tensors_data() -> tp.Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+    """Converts dataset into PyTorch tensors for training and validation.
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+    Returns:
+        tp.Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]: Tensors for training and validation.
+    """
+    X_train, X_valid, y_train, y_valid = make_dataset(RAW_DATA_PATH)
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+    X_train_tensor = torch.tensor(X_train.values, dtype=torch.float32)
+    y_train_tensor = torch.tensor(y_train.values, dtype=torch.float32)
+    X_valid_tensor = torch.tensor(X_valid.values, dtype=torch.float32)
+    y_valid_tensor = torch.tensor(y_valid.values, dtype=torch.float32)
 
-    main()
+    return X_train_tensor, X_valid_tensor, y_train_tensor, y_valid_tensor
